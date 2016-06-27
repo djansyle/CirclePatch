f69b182d18d83c7ed3f814458b1f45167e64cbf0
djansyle <djansyledjans@gmail.com>
2016-06-27 13:40:49 +0800

    fix(url): fix package url

--- mysql-5.6.sh
+++ mysql-5.6.sh
@@ -6,14 +6,14 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -LO https://dev.mysql.com/get/mysql-apt-config_0.5.3-1_all.deb1
+curl -LO http://repo.mysql.com//mysql-apt-config_0.6.3-1_all.deb
 echo mysql-apt-config mysql-apt-config/select-product select Apply | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.6 | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-connector-python select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-workbench select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-utilities select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-connector-odbc select connector-odbc-x.x | sudo debconf-set-selections
-sudo -E dpkg -i mysql-apt-config_0.5.3-1_all.deb
+sudo -E dpkg -i mysql-apt-config_0.6.3-1_all.deb
 sudo apt-get update
 echo mysql-community-server mysql-community-server/re-root-pass password ${mysql_root_password} | sudo debconf-set-selections
 echo mysql-community-server mysql-community-server/root-pass password ${mysql_root_password} | sudo debconf-set-selections
