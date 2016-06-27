23e8c9c43bd42cbd18e2343cd8d785ace7d16204
djansyle <djansyledjans@gmail.com>
2016-06-27 13:51:42 +0800

    fix(version): remove old version scripts

--- mysql-5.6.sh
+++ mysql-5.6.sh
@@ -6,18 +6,16 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -LO http://repo.mysql.com//mysql-apt-config_0.6.3-1_all.deb
+curl -LO http://repo.mysql.com//mysql-apt-config_0.7.3-1_all.deb
 echo mysql-apt-config mysql-apt-config/select-product select Apply | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.6 | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-connector-python select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-workbench select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-utilities select none | sudo debconf-set-selections
 echo mysql-apt-config mysql-apt-config/select-connector-odbc select connector-odbc-x.x | sudo debconf-set-selections
-sudo -E dpkg -i mysql-apt-config_0.6.3-1_all.deb
+sudo -E dpkg -i mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update
-echo mysql-community-server mysql-community-server/re-root-pass password ${mysql_root_password} | sudo debconf-set-selections
-echo mysql-community-server mysql-community-server/root-pass password ${mysql_root_password} | sudo debconf-set-selections
-sudo -E apt-get -y install mysql-community-server
+sudo -E apt-get -y install mysql-server-5.6
 
 echo "Checking installed version....."
 mysql -D mysql -e "SELECT version()"
